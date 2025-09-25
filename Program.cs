e7c2861150a128e7c8b4ed73285923b0a0926d74
paranoidromulan <veronylie@gmail.com>
2025-09-25 13:12:32 +0300

    Added functions
    Now can search by todays date, yesterdays date and a random date. Also made downloading only if image a method

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿using System.Net.Http.Headers;
+﻿using System;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Reflection;
 using HttpClient client = new();
 
+
 client.DefaultRequestHeaders.Accept.Clear();
+Random rand = new Random();
+DateTime today = DateTime.Today;
 while (true)
 {
 
@@ -13,13 +17,53 @@ while (true)
 
     if (response == "1")
     {
-        Console.WriteLine("Please insert the date to download an image (for example 2023-06-06)");
-        string date = Console.ReadLine();
-        var apodResponse = await ProcessApodAsync(client, date);
+        Console.WriteLine("Would you like to provide a date(1), donwload the image of today(2), download the image of yesterday (3) or choose a random date(4)");
+        string howtosearch = Console.ReadLine();
 
-        await DownloadImageAsync(client, apodResponse);
+        if (howtosearch == "1")
+        {
+            Console.WriteLine("Please insert the date to download an image (for example 2023-06-06)");
+            string date = Console.ReadLine();
 
+            var apodResponse = await ProcessApodAsync(client, date);
 
+            DownloadIfImageAsync(client, apodResponse);
+        }
+        else if (howtosearch == "2")
+        {
+            string todaydate = DateTime.Today.ToString("yyyy-MM-dd");
+            var apodToday = await ProcessApodAsync(client, todaydate);
+
+            DownloadIfImageAsync(client, apodToday);
+
+        }
+        else if (howtosearch == "3")
+        {
+            string yesterday = DateTime.Today.AddDays(-1).ToString("yyyy-MM-dd");
+            var apodYesterday = await ProcessApodAsync(client, yesterday);
+
+            DownloadIfImageAsync(client, apodYesterday);
+        }
+        else if (howtosearch == "4")
+        {
+            DateTime start = new DateTime(1995, 6, 16);
+            DateTime end = today;
+
+            int rangeDays = (end - start).Days;
+            DateTime randomDate = start.AddDays(rand.Next(rangeDays + 1));
+
+            Console.WriteLine($"Random date: {randomDate:yyyy-MM-dd}");
+            string randomDateString = randomDate.ToString("yyyy-MM-dd");
+
+            var apodRandom = await ProcessApodAsync(client, randomDateString);
+
+            DownloadIfImageAsync(client, apodRandom);
+
+        }
+        else
+        {
+            Console.WriteLine("Please provide a valid option");
+        }
     }
     else if (response == "2")
     {
@@ -31,9 +75,16 @@ while (true)
     }
 
 }
+async Task DownloadIfImageAsync(HttpClient client, ApodResponse apod)
+{
+    if (apod != null && apod.Media_Type == "image")
+        await DownloadImageAsync(client, apod);
+    else
+        Console.WriteLine("No image found (maybe it's a video)");
+}
 static async Task<ApodResponse> ProcessApodAsync(HttpClient client, string date)
 {
-    string url = $"https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date={date}";
+    string url = $"https://api.nasa.gov/planetary/apod?api_key=eHVVF1gTlDryhpxq8qDRh3cddhdPHSWK4cE6faWk&date={date}";
     try
     {
         var apod = await client.GetFromJsonAsync<ApodResponse>(url);
